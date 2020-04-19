3821a320b1aad0caa386bc5c9acadbb1de69c616
dwikiriswanda <m.dwikiriswanda@gmail.com>
2020-04-19 17:02:50 +0700

    done

--- Tugas/Tugas 5/Graph Berarah Tipe 2/header.h
+++ Tugas/Tugas 5/Graph Berarah Tipe 2/header.h
@@ -11,18 +11,15 @@ using namespace std;
 typedef char infotype;
 typedef struct vertek *adrVertek;
 typedef struct edge *adrEdge;
-
 struct vertek {
  infotype info;
  adrVertek next;
  adrEdge firstEdge;
 };
-
 struct edge {
  adrVertek info;
  adrEdge next;
 };
-
 void createGraph(adrVertek &Graph);
 adrVertek alokasiVertek(infotype label);
 adrEdge alokasiEdge();
